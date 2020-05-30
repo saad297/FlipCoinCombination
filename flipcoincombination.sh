35c67024eb63bdf299854653e3e6ef551d7bc205
“saad297” <skamil60@gmail.com>
2020-05-30 09:17:37 +0530

    Added : Doublet Code For Head And Tails

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,15 +1,18 @@
 echo "Welcome to the flip coin combination program."
 
 read -p "Enter the number of times you want to flip : " a
+read -p "Enter the choice of combination :
+1) Singlet
+2) Doublet  " b
 
-declare -A singlet
+declare -A dictionary
 
 
 for (( i=1;i<=$a;i++ ))
 do
 
 	combination=""
-	for (( j=1;j<=1;j++ ))
+	for (( j=1;j<=$b;j++ ))
 	do
 
 		x=$(($RANDOM%2))
@@ -21,14 +24,45 @@ do
 		combination=$combination"T"
 	fi
 	done
-	singlet[$combination]=$((${singlet[$combination]}+1))
+	dictionary[$combination]=$((${dictionary[$combination]}+1))
 done
 
-if [ ${singlet[H]} -gt  ${singlet[T]} ]
+if [ $b -eq 1 ]
 then
-	ans=$((${singlet[H]}*100/$a))
-	echo "Heads =  ${singlet[H]} %"
-else
-	ans=$((${singlet[T]}*100/$a))
-        echo "Tails =  ${singlet[T]} %"
+		if [ ${dictionary[H]} -gt  ${dictionary[T]} ]
+		then
+			ans=$((${dictionary[H]}*100/$a))
+			echo "Heads =  ${dictionary[H]} %"
+		else
+			ans=$((${dictionary[T]}*100/$a))
+			echo "Tails =  ${dictionary[T]} %"
+		fi
+		
+elif [ $b -eq 2 ]
+then
+
+		if [ ${dictionary[HH]} -gt  ${dictionary[TT]} ] && [ ${dictionary[HH]} -gt ${dictionary[TH]} ] && [ ${dictionary[HH]} -gt ${dictionary[HT]} ]
+		then
+			ans=$((${dictionary[HH]}*100/$a))
+			echo "HH =  ${dictionary[HH]} %"
+
+		elif  [ ${dictionary[TT]} -gt  ${dictionary[HH]} ] && [ ${dictionary[TT]} -gt ${dictionary[TH]} ] && [ ${dictionary[TT]} -gt ${dictionary[HT]} ]
+		then
+
+			ans=$((${dictionary[TT]}*100/$a))
+			echo "TT =  ${dictionary[TT]} %"
+
+		elif  [ ${dictionary[TH]} -gt  ${dictionary[TT]} ] && [ ${dictionary[TH]} -gt ${dictionary[HH]} ] && [ ${dictionary[TH]} -gt ${dictionary[HT]} ]
+		then
+
+			ans=$((${dictionary[TH]}*100/$a))
+         echo "TH =  ${dictionary[TH]} %"
+
+		else
+
+			ans=$((${dictionary[HT]}*100/$a))
+         echo "HT =  ${dictionary[HT]} %"
+
+		fi
+		
 fi
